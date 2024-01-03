f9d327e0caced3989d983162ad6c465ed742e598
Amirshah98760 <amirshah98760@gmail.com>
2024-01-03 14:01:34 +0500

    Gpa program

---
+++ script.js
@@ -0,0 +1,398 @@
+const englishMarks = +prompt("Enter your English Marks  ");
+const urduMarks = +prompt("Enter your Urdu Marks ");
+const physicsMarks = +prompt("Enter your Physics Marks");
+const ictpaper = +prompt("Enter your ictpaper Marks");
+const calculus = +prompt("Enter your Calculus Marks");
+const programming = +prompt("Enter your Programming Marks");
+
+if(englishMarks >= 90){
+    console.log("Your English Marks is " + englishMarks);
+    console.log("Your Gpa is 4 in this Subject");
+    console.log("Your grade is A+");
+}
+else if(englishMarks >=85 && englishMarks <= 89){
+    console.log("Your English Marks is " + englishMarks);
+    console.log("Your Gpa is 4 in this Subject");
+    console.log("Your grade is A");
+}
+else if(englishMarks >=80 && englishMarks <= 84){
+    console.log("Your English Marks is " + englishMarks);
+    console.log("Your Gpa is 3.8 in this Subject");
+    console.log("Your grade is A-");
+}
+else if(englishMarks >=75 && englishMarks <=79){
+    console.log("Your English Marks is " + englishMarks);
+    console.log("Your Gpa is 3.4 in this Subject");
+    console.log("Your grade is B+");
+}
+else if(englishMarks >=71 && englishMarks <= 74){
+    console.log("Your English Marks is " + englishMarks);
+    console.log("Your Gpa is 3.0 in this Subject");
+    console.log("Your grade is B");
+}
+else if(englishMarks >=68 && englishMarks <= 70){
+    console.log("Your English Marks is " + englishMarks);
+    console.log("Your Gpa is 2.8 in this Subject");
+    console.log("Your grade is B-");
+}
+else if(englishMarks >=64 && englishMarks <= 67){
+    console.log("Your English Marks is " + englishMarks);
+    console.log("Your Gpa is 2.4 in this Subject");
+    console.log("Your grade is C+");
+}
+else if(englishMarks >=61 && englishMarks <= 63){
+    console.log("Your English Marks is " + englishMarks);
+    console.log("Your Gpa is 2.0 in this Subject");
+    console.log("Your grade is C");
+}
+else if(englishMarks >=57 && englishMarks <= 60){
+    console.log("Your English Marks is " + englishMarks);
+    console.log("Your Gpa is 1.8 in this Subject");
+    console.log("Your grade is C-");
+}
+else if(englishMarks >=53 && englishMarks <=56){
+    console.log("Your English Marks is " + englishMarks);
+    console.log("Your Gpa is 1.4 in this Subject");
+    console.log("Your grade is D+");
+}
+else if(englishMarks >=50 && englishMarks <= 52){
+    console.log("Your English Marks is " + englishMarks);
+    console.log("Your Gpa is 1.0 in this Subject");
+    console.log("Your grade is D");
+}
+else{
+    console.log("Your English Marks is " + englishMarks);
+    console.log("You are Fail ");
+}
+
+
+
+if(urduMarks >= 90){
+    console.log("Your Urdu Marks is " + urduMarks);
+    console.log("Your Gpa is 4 in this Subject");
+    console.log("Your grade is A+");
+}
+else if(urduMarks >=85 && urduMarks <= 89){
+    console.log("Your Urdu Marks is " + urduMarks);
+    console.log("Your Gpa is 4 in this Subject");
+    console.log("Your grade is A");
+}
+else if(urduMarks >=80 && urduMarks <= 84){
+    console.log("Your Urdu Marks is " + urduMarks);
+    console.log("Your Gpa is 3.8 in this Subject");
+    console.log("Your grade is A-");
+}
+else if(urduMarks >=75 && urduMarks <=79){
+    console.log("Your Urdu Marks is " + urduMarks);
+    console.log("Your Gpa is 3.4 in this Subject");
+    console.log("Your grade is B+");
+}
+else if(urduMarks >=71 && englishMarks <= 74){
+    console.log("Your Urdu Marks is " + urduMarks);
+    console.log("Your Gpa is 3.0 in this Subject");
+    console.log("Your grade is B");
+}
+else if(urduMarks >=68 && urduMarks <= 70){
+    console.log("Your Urdu Marks is " + urduMarks);
+    console.log("Your Gpa is 2.8 in this Subject");
+    console.log("Your grade is B-");
+}
+else if(urduMarks >=64 && urduMarks <= 67){
+    console.log("Your Urdu Marks is " + urduMarks);
+    console.log("Your Gpa is 2.4 in this Subject");
+    console.log("Your grade is C+");
+}
+else if(urduMarks >=61 && urduMarks <= 63){
+    console.log("Your Urdu Marks is " + urduMarks);
+    console.log("Your Gpa is 2.0 in this Subject");
+    console.log("Your grade is C");
+}
+else if(urduMarks >=57 && urduMarks <= 60){
+    console.log("Your Urdu Marks is " + urduMarks);
+    console.log("Your Gpa is 1.8  in this Subject");
+    console.log("Your grade is C-");
+}
+else if(urduMarks >=53 && urduMarks <=56){
+    console.log("Your Urdu Marks is " + urduMarks);
+    console.log("Your Gpa is 1.4 in this Subject");
+    console.log("Your grade is D+");
+}
+else if(urduMarks >=50 && urduMarks >= 52){
+    console.log("Your Urdu Marks is " + urduMarks);
+    console.log("Your Gpa is 1.0 in this Subject");
+    console.log("Your grade is D");
+}
+else{
+    console.log("Your Urdu Marks is " + urduMarks);
+    console.log("You are Fail ");
+}
+
+
+
+if(physicsMarks >= 90){
+    console.log("Your Physics Marks is " + physicsMarks);
+    console.log("Your Gpa is 4 in this Subject");
+    console.log("Your grade is A+");
+}
+else if(physicsMarks >=85 && physicsMarks <= 89){
+    console.log("Your Physics Marks is " + physicsMarks);
+    console.log("Your Gpa is 4 in this Subject");
+    console.log("Your grade is A");
+}
+else if(physicsMarks >=80 && physicsMarks <= 84){
+    console.log("Your Physics Marks is " + physicsMarks);
+    console.log("Your Gpa is 3.8 in this Subject");
+    console.log("Your grade is A-");
+}
+else if(physicsMarks >=75 && physicsMarks <=79){
+    console.log("Your Physics Marks is " + physicsMarks);
+    console.log("Your Gpa is 3.4 in this Subject");
+    console.log("Your grade is B+");
+}
+else if(physicsMarks >=71 && physicsMarks <= 74){
+    console.log("Your Physics Marks is " + physicsMarks);
+    console.log("Your Gpa is 3.0 in this Subject");
+    console.log("Your grade is B");
+}
+else if(physicsMarks >=68 && physicsMarks <= 70){
+    console.log("Your Physics Marks is " + physicsMarks);
+    console.log("Your Gpa is 2.8  in this Subject");
+    console.log("Your grade is B-");
+}
+else if(physicsMarks >=64 && physicsMarks <= 67){
+    console.log("Your Physics Marks is " + physicsMarks);
+    console.log("Your Gpa is 2.4 in this Subject");
+    console.log("Your grade is C+");
+}
+else if(physicsMarks >=61 && physicsMarks <= 63){
+    console.log("Your Physics Marks is " + physicsMarks);
+    console.log("Your Gpa is 2.0 in this Subject");
+    console.log("Your grade is C");
+}
+else if(physicsMarks >=57 && physicsMarks <= 60){
+    console.log("Your Physics Marks is " + physicsMarks);
+    console.log("Your Gpa is 1.8  in this Subject");
+    console.log("Your grade is C-");
+}
+else if(physicsMarks >=53 && physicsMarks <=56){
+    console.log("Your Physics Marks is " + physicsMarks);
+    console.log("Your Gpa is 1.4  in this Subject");
+    console.log("Your grade is D+");
+}
+else if(physicsMarks >=50 && physicsMarks <= 52){
+    console.log("Your Physics Marks is " + physicsMarks);
+    console.log("Your Gpa is 1.0 in this Subject");
+    console.log("Your grade is D");
+}
+else{
+    console.log("Your Physics Marks is " + physicsMarks);
+    console.log("You are Fail ");
+}
+
+
+if(ictpaper >= 90){
+    console.log("Your ictpaper Marks is " + ictpaper);
+    console.log("Your Gpa is 4 in this Subject");
+    console.log("Your grade is A+");
+}
+else if(ictpaper >=85 && ictpaper <= 89){
+    console.log("Your ictpaper Marks is " + ictpaper);
+    console.log("Your Gpa is 4 in this Subject");
+    console.log("Your grade is A");
+}
+else if(ictpaper >=80 && ictpaper <= 84){
+    console.log("Your Physics Marks is " + ictpaper);
+    console.log("Your Gpa is 3.8 in this Subject");
+    console.log("Your grade is A-");
+}
+else if(ictpaper >=75 && ictpaper <=79){
+    console.log("Your Physics Marks is " + ictpaper);
+    console.log("Your Gpa is 3.4 in this Subject");
+    console.log("Your grade is B+");
+}
+else if(ictpaper >=71 && ictpaper <= 74){
+    console.log("Your Physics Marks is " + ictpaper);
+    console.log("Your Gpa is 3.0 in this Subject");
+    console.log("Your grade is B");
+}
+else if(ictpaper >=68 && ictpaper <= 70){
+    console.log("Your Physics Marks is " + ictpaper);
+    console.log("Your Gpa is 2.8  in this Subject");
+    console.log("Your grade is B-");
+}
+else if(ictpaper >=64 && ictpaper <= 67){
+    console.log("Your Physics Marks is " + ictpaper);
+    console.log("Your Gpa is 2.4 in this Subject");
+    console.log("Your grade is C+");
+}
+else if(ictpaper >=61 && ictpaper <= 63){
+    console.log("Your Physics Marks is " + ictpaper);
+    console.log("Your Gpa is 2.0 in this Subject");
+    console.log("Your grade is C");
+}
+else if(ictpaper >=57 && ictpaper <= 60){
+    console.log("Your Physics Marks is " + ictpaper);
+    console.log("Your Gpa is 1.8  in this Subject");
+    console.log("Your grade is C-");
+}
+else if(ictpaper >=53 && ictpaper <=56){
+    console.log("Your Physics Marks is " + ictpaper);
+    console.log("Your Gpa is 1.4  in this Subject");
+    console.log("Your grade is D+");
+}
+else if(ictpaper >=50 && ictpaper <= 52){
+    console.log("Your Physics Marks is " + ictpaper);
+    console.log("Your Gpa is 1.0 in this Subject");
+    console.log("Your grade is D");
+}
+else{
+    console.log("Your Physics Marks is " + ictpaper);
+    console.log("You are Fail ");
+}
+
+
+if(calculus >= 90){
+    console.log("Your Calculus Marks is " + calculus);
+    console.log("Your Gpa is 4 in this Subject");
+    console.log("Your grade is A+");
+}
+else if(calculus >=85 && calculus <= 89){
+    console.log("Your Calculus Marks is " + calculus);
+    console.log("Your Gpa is 4 in this Subject");
+    console.log("Your grade is A");
+}
+else if(calculus >=80 && calculus <= 84){
+    console.log("Your Calculus Marks is " + calculus);
+    console.log("Your Gpa is 3.8 in this Subject");
+    console.log("Your grade is A-");
+}
+else if(calculus >=75 && calculus <=79){
+    console.log("Your Calculus Marks is " + calculus);
+    console.log("Your Gpa is 3.4 in this Subject");
+    console.log("Your grade is B+");
+}
+else if(calculus >=71 && calculus <= 74){
+    console.log("Your Calculus Marks is " + calculus);
+    console.log("Your Gpa is 3.0 in this Subject");
+    console.log("Your grade is B");
+}
+else if(calculus >=68 && calculus <= 70){
+    console.log("Your Calculus Marks is " + calculus);
+    console.log("Your Gpa is 2.8  in this Subject");
+    console.log("Your grade is B-");
+}
+else if(calculus >=64 && calculus <= 67){
+    console.log("Your Calculus Marks is " + calculus);
+    console.log("Your Gpa is 2.4 in this Subject");
+    console.log("Your grade is C+");
+}
+else if(calculus >=61 && calculus <= 63){
+    console.log("Your Calculus Marks is " + calculus);
+    console.log("Your Gpa is 2.0 in this Subject");
+    console.log("Your grade is C");
+}
+else if(calculus >=57 && calculus <= 60){
+    console.log("Your Calculus Marks is " + calculus);
+    console.log("Your Gpa is 1.8  in this Subject");
+    console.log("Your grade is C-");
+}
+else if(calculus >=53 && calculus <=56){
+    console.log("Your Calculus Marks is " + calculus);
+    console.log("Your Gpa is 1.4  in this Subject");
+    console.log("Your grade is D+");
+}
+else if(calculus >=50 && calculus <= 52){
+    console.log("Your Calculus Marks is " + calculus);
+    console.log("Your Gpa is 1.0 in this Subject");
+    console.log("Your grade is D");
+}
+else{
+    console.log("Your Calculus  Marks is " + calculus);
+    console.log("You are Fail ");
+}
+
+
+if(programming >= 90){
+    console.log("Your Programming Marks is " + programming);
+    console.log("Your Gpa is 4 in this Subject");
+    console.log("Your grade is A+");
+}
+else if(programming >=85 && programming <= 89){
+    console.log("Your Programming Marks is " + programming);
+    console.log("Your Gpa is 4 in this Subject");
+    console.log("Your grade is A");
+}
+else if(programming >=80 && programming <= 84){
+    console.log("Your Programming Marks is " + programming);
+    console.log("Your Gpa is 3.8 in this Subject");
+    console.log("Your grade is A-");
+}
+else if(programming >=75 && programming <=79){
+    console.log("Your Programming Marks is " + programming);
+    console.log("Your Gpa is 3.4 in this Subject");
+    console.log("Your grade is B+");
+}
+else if(programming >=71 && programming <= 74){
+    console.log("Your Programming Marks is " + programming);
+    console.log("Your Gpa is 3.0 in this Subject");
+    console.log("Your grade is B");
+}
+else if(programming >=68 && programming <= 70){
+    console.log("Your Programming Marks is " + programming);
+    console.log("Your Gpa is 2.8  in this Subject");
+    console.log("Your grade is B-");
+}
+else if(programming >=64 && programming <= 67){
+    console.log("Your Programming Marks is " + programming);
+    console.log("Your Gpa is 2.4 in this Subject");
+    console.log("Your grade is C+");
+}
+else if(programming >=61 && programming <= 63){
+    console.log("Your Programming Marks is " + programming);
+    console.log("Your Gpa is 2.0 in this Subject");
+    console.log("Your grade is C");
+}
+else if(programming >=57 && programming <= 60){
+    console.log("Your Programming Marks is " + calculus);
+    console.log("Your Gpa is 1.8  in this Subject");
+    console.log("Your grade is C-");
+}
+else if(programming >=53 && programming <=56){
+    console.log("Your Programming Marks is " + programming);
+    console.log("Your Gpa is 1.4  in this Subject");
+    console.log("Your grade is D+");
+}
+else if(programming >=50 && programming <= 52){
+    console.log("Your Programming Marks is " + programming);
+    console.log("Your Gpa is 1.0 in this Subject");
+    console.log("Your grade is D");
+}
+else{
+    console.log("Your Programming  Marks is " + programming);
+    console.log("You are Fail ");
+}
+
+
+
+
+let result;
+result = englishMarks + urduMarks + physicsMarks + ictpaper + calculus + programming;
+
+console.log("Your total Marks is = " + result );
+console.log("Your Percentage  is  " + result / 600 * 100 +" " + "%");
+
+const firstSubjectGpa = +prompt("Please Enter Your First subject Gpa ");
+const secondSubjectGpa = +prompt("Please Enter Your Second  subject Gpa ");
+const thirdSubjectGpa = +prompt("Please Enter Your Third subject Gpa ");
+const fourthSubjectGpa = +prompt("Please Enter Your Fourth subject Gpa ");
+const fifthSubjectGpa = +prompt("Please Enter Your Fifth subject Gpa ");
+const sixthSubjectGpa = +prompt("Please Enter Your Sixth subject Gpa ");
+
+const obtainGpa = firstSubjectGpa + secondSubjectGpa + thirdSubjectGpa + fourthSubjectGpa + fifthSubjectGpa;
+
+console.log("Your Total Gpa is:  " +  obtainGpa / 24 * 6 );
+
+
+
+
+
